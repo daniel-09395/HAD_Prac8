623732b139736229fde99cad06c79850fa73b0ef
daniel-09395 <60725684+daniel-09395@users.noreply.github.com>
2021-02-15 22:46:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+//import ToggleDarkLight from './ToggleDarkLight'; //Pract7
+//import Button from './CssClassName'; //Pract2.2
+//import Form from './Form'; //Pract6
+//import { DropDownTextJS, DropDownTextState} from './DropDownText';
+//import Parent from './Parent_to_child'; //Pract3.1
+//import Parent from './Child_to_parent'; //Pract3.2
+//import ListValue from './ListIterator'; //Pract4
+//import MagicShop from './MagicShop'; //Pract8.1
+//import MagicShopUseEffect from './MagicShopUseEffect'; //Pract8.2
+
+function App() {
+	return (
+		//<ToggleDarkLight />
+		//<Button />
+		//<Form />
+		//<React.StrictMode>
+		//<DropDownTextJS /> 
+		//<DropDownTextState />
+		//</React.StrictMode>
+		//<Parent />
+		//<ListValue />
+		//<MagicShop />
+		//<MagicShopUseEffect />
+	)
+}
+	
+export default App;
